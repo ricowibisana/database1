43404ec7bf92e393f51028532aaffceb5b3e56c8
ricowibisana <ricowibisana69@gmail.com>
2020-02-12 09:07:34 +0700

    DB

--- dbproduct .sql
+++ dbproduct .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Feb 2020 pada 04.02
+-- Waktu pembuatan: 12 Feb 2020 pada 03.06
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -62,7 +62,13 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `nama`, `harga`, `jumlah`, `image`, `created_at`, `updated_at`) VALUES
-(2, 'Lumba-Lumba', '20000', '20', '', '2020-02-05 22:59:01', '2020-02-05 22:59:16');
+(3, 'Buku', '40000', '20', '20200211090908.png', '2020-02-11 02:09:08', '2020-02-11 02:09:08'),
+(4, 'Laptop', '9000000', '20', '20200211090952.png', '2020-02-11 02:09:52', '2020-02-11 02:09:52'),
+(5, 'Stokes-Hermann', '37328', '10', 'd81f3bda8cf908dc81b7b89a2c1e389f.jpg', '2020-02-11 18:58:31', '2020-02-11 18:58:31'),
+(6, 'Weimann-Koch', '44293', '10', '7df2497f21b544510afc0f787cc9916d.jpg', '2020-02-11 18:58:31', '2020-02-11 18:58:31'),
+(7, 'Kutch-Bahringer', '40855', '10', 'ea6aac84ebc8004e6fbeb7f5019ea795.jpg', '2020-02-11 18:58:31', '2020-02-11 18:58:31'),
+(8, 'Harber-Wolff', '39479', '10', 'ab4d6f4d7a09e90ffce8de1eb7779675.jpg', '2020-02-11 18:58:31', '2020-02-11 18:58:31'),
+(9, 'Barrows-Leuschke', '32955', '10', '1581f127bdb2d0552832a97c20c8ad3f.jpg', '2020-02-11 18:58:32', '2020-02-11 18:58:32');
 
 --
 -- Indexes for dumped tables
@@ -94,7 +100,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
