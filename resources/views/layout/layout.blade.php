d16274f4e5392e2f3077963d56aa301f18de2961
ricowibisana <ricowibisana69@gmail.com>
2020-02-24 17:34:00 +0700

    new_template

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -39,6 +39,21 @@
 					<input type="text" name="cari" placeholder="Cari Product .." value="{{ old('cari') }}">
 					<input type="submit" value="CARI">
 				</form>
+        <div class="main-sidebar">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+          </div>
+          <ul class="sidebar-menu">
+              <li class="menu-header">Dashboard</li>
+              <li><a class="nav-link" href="/welcome"><i class="far fa-square"></i> <span>Home</span></a></li>
+              <li><a class="nav-link" href="/product"><i class="far fa-square"></i> <span>Product</span></a></li>
+              <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>About</span></a></li>
+                </ul>
+              </li>
             </div>
           </div>
 
